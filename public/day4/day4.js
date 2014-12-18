9a64c232c4fab09ccf05d9fa518af9e1b7548692
Stacey Lin <twstacey@gmail.com>
2014-12-17 22:20:28 -0800

    Day 4 lights off

---
+++ public/day4/day4.js
@@ -0,0 +1,50 @@
+var clicks = 0 
+
+function lights(){
+  clicks = clicks + 1
+  switch(clicks){
+    case 1: 
+      document.getElementById('story').innerHTML = "Ahh! What's that? Turn on the lights!";
+      document.getElementById('eyeball').style.visibility = 'visible';
+      togglelights("off");
+      break;
+    case 2:
+      document.getElementById('story').innerHTML = "Huh, where did it go? Guess it was just a trick of the light."
+      document.getElementById('eyeball').style.visibility = 'hidden';
+      togglelights("on");
+      break;
+    case 3:
+      document.getElementById('story').innerHTML = "Arghhhh! There it is again! Lights, lights!";
+      document.getElementById('eyeball').style.visibility = 'visible';
+      togglelights("off");
+      break;
+    case 4:
+      document.getElementById('story').innerHTML = "A monster!! Click to capture him!!";
+      document.getElementById('eyeball').style.visibility = 'hidden';
+      document.getElementById('monster').style.visibility = 'visible';
+      togglelights("on");
+      break;
+    default: 
+      if (document.body.style.backgroundColor === "white"){
+        togglelights("off")
+      } else {
+        togglelights("on")
+      }
+  }
+}
+
+function disappear(){
+  document.getElementById('monster').style.visibility = 'hidden';
+  document.getElementById('story').innerHTML = "Inspired by Lights Off, a short film. <br> (Psst, watch it with the lights off!)";
+  document.getElementById('video').style.visibility = 'visible';
+}
+
+function togglelights(status){
+  if (status === "on"){
+    document.getElementById('story').style.color = "black";
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.getElementById('story').style.color = "white";
+    document.body.style.backgroundColor = "black";
+  }
+}
