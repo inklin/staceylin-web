0da76f1b75ad45682d5fb15e3034a25d3eb57e65
Stacey Lin <twstacey@gmail.com>
2014-12-19 22:22:18 -0700

    Added Play Again feature

--- public/guessanumber/guessanumber.js
+++ public/guessanumber/guessanumber.js
@@ -1,30 +1,37 @@
-var x = Math.floor(Math.random() * 100);
+var x = Math.floor((Math.random() * 100) + 1);
 
 var turns = 5;
 
 function guessNumber() {
-  var guess = document.getElementById("guess");
-  guess = parseInt(guess);
+  var guess = parseInt(document.getElementById("guess").value);
   if (guess === x) {
-    document.getElementById("result").innerHTML = "You win!"
+    document.getElementById("result").innerHTML = "You win! " + guess + "is the answer!";
     turns = 0; 
     document.getElementById("turns").innerHTML = "You have 0 guesses left.";
   } else {
     if (guess < x) {
-    document.getElementById("result").innerHTML = "Hint: Too small!";
+      document.getElementById("result").innerHTML = "Sorry! " + guess + " is too small!";
     }
     if (guess > x) {
-    document.getElementById("result").innerHTML = "Hint: Too big!";
+      document.getElementById("result").innerHTML = "Sorry! " + guess + " is too big!";
     }
     turns = turns - 1;
     document.getElementById("turns").innerHTML = "You have " + turns + " guesses left.";
     }
   
   if (turns === 0 && guess != x){
-    document.getElementById("turns").innerHTML = "Game Over. You're out of guesses! The answer is " + x + " !";
-    document.getElementById("userguess").style.visibility = "hidden";
+    document.getElementById("result").style.visibility = "hidden";
+    document.getElementById("turns").innerHTML = "Game Over. You're out of guesses! The answer is " + x + ".";
+    document.getElementById("userguess").style.visibility = 'hidden';
+    document.getElementById("playagain").style.visibility = 'visible';
   }
 }
 
+function playAgain() {
+  var turns = 5;
+  document.getElementById("userguess").style.visibility = 'visible';
+  document.getElementById("turns").innerHTML = "You have 5 guesses. Good luck!";
+  document.getElementById("playagain").style.visibility = 'hidden';
+}
 
 
