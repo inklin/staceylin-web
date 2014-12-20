0876634e39e287068055fe98bf01ea21cb0f9ab2
Stacey Lin <twstacey@gmail.com>
2014-12-20 14:16:20 -0700

    Updated style of guess a number

--- public/guessanumber/guessanumber.js
+++ public/guessanumber/guessanumber.js
@@ -4,13 +4,13 @@ var turns = 5;
 
 function guessNumber() {
   if (document.getElementById("guess").value === '') {
-    return;
-  }
+    return;
+  }
   var guess = parseInt(document.getElementById("guess").value);
   if (guess === x) {
     document.getElementById("result").innerHTML = "You win! " + guess + " is the answer!";
-    document.getElementById("userguess").style.visibility = 'hidden';
-    document.getElementById("playagain").style.visibility = 'visible';
+    document.getElementById("userguess").style.display = 'none';
+    document.getElementById("playagain").style.display = 'block';
     turns = 0; 
     document.getElementById("turns").innerHTML = "You have 0 guesses left.";
   } else {
@@ -25,20 +25,22 @@ function guessNumber() {
     }
   
   if (turns === 0 && guess != x){
-    document.getElementById("result").style.visibility = "hidden";
+    document.getElementById("result").style.display = "none";
     document.getElementById("turns").innerHTML = "Game Over. You're out of guesses! The answer is " + x + ".";
-    document.getElementById("userguess").style.visibility = 'hidden';
-    document.getElementById("playagain").style.visibility = 'visible';
+    document.getElementById("userguess").style.display = 'none';
+    document.getElementById("playagain").style.display = 'block';
   }
 }
 
 function playAgain() {
-  turns = 5;
-  x = Math.floor((Math.random() * 100) + 1);
-  document.getElementById("userguess").style.visibility = 'visible';
-  document.getElementById("turns").innerHTML = "You have 5 guesses. Good luck!";
-  document.getElementById("playagain").style.visibility = 'hidden';
-  document.getElementById("guess").value = ""
+  turns = 5;
+  x = Math.floor((Math.random() * 100) + 1);
+  document.getElementById("result").style.display = "block";
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("userguess").style.display = 'block';
+  document.getElementById("turns").innerHTML = "You have 5 guesses. Good luck!";
+  document.getElementById("playagain").style.display = 'none';
+  document.getElementById("guess").value = "";
 }
 
 
