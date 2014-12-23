032c999a25a87950ad56dd84fb7775119ae0ec4a
Stacey Lin <twstacey@gmail.com>
2014-12-23 00:19:48 -0700

    Added piano sounds to Rainbow Piano

--- public/rainbowpiano/scripts.js
+++ public/rainbowpiano/scripts.js
@@ -1,27 +1,21 @@
+"use strict";
+
 $(document).ready(function(){
   
-  var rainbowIndex = 0 
+  var rainbowIndex = 0;
 
-  /* Function that gives you a color value related to sine frequences -> cyclical */ 
+  /* Function that gives you a color value related to sine frequences -> cyclical */
   function getColor(i) {
-    var frequency = 0.5
-    var red = Math.sin(frequency*i + 0)* 103 + 152;
-    var green = Math.sin(frequency*i + 2)* 103 + 152;
-    var blue = Math.sin(frequency*i + 4)* 103 + 152;
+    var frequency = 0.5;
+    var red = Math.sin(frequency*i + 0)* 128 + 127;
+    var green = Math.sin(frequency*i + 2)* 128 + 127;
+    var blue = Math.sin(frequency*i + 4)* 128 + 127;
 
-    /* Function that converts byte to hex values */
-    var hexChar = ["0", "1", "2", "3", "4", "5", "6", "7","8", "9", "A", "B", "C", "D", "E", "F"];
-    
-    function byteToHex(value) {
-      return hexChar[(value >> 4) & 0x0f] + hexChar[value & 0x0f];
-    }
-
-    /* return a color value for html */ 
-    return "#" + byteToHex(red) + byteToHex(green) + byteToHex(blue);
-  };
+    return 'rgb('+ Math.round(red) +','+ Math.round(green) +','+ Math.round(blue) +')';
+  }
 
   /* When the mouse goes over a key, change the background color, 
-  increment the index value so that you get a different color the next time*/ 
+  increment the index value so that you get a different color the next time*/
 
   $(".key").hover( function(){
     $(this).css("background-color", getColor(rainbowIndex));
@@ -30,6 +24,16 @@ $(document).ready(function(){
       $(this).css("background-color", "");
     });
 
+  var sound = null;
+  $(".key").mouseenter( function(){
+    var nsound = Math.ceil(Math.random() *12);
+    while (nsound === sound){
+      nsound = Math.ceil(Math.random() *12);
+    }
+
+    $("#sound-" + nsound)[0].play();
+    sound = nsound;
+  });
 
 });
 
