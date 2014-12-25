61b7f5ace22ed5b8e62d8f671986db325c29a239
Stacey Lin <twstacey@gmail.com>
2014-12-24 23:54:10 -0700

    Add beginning of Day 11

---
+++ public/greetingcard/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#inputgreeting").click(function(){
+    var greeting = $("#greeting").val();
+    if (greeting === "") {
+      alert("You haven't entered a greeting yet!");
+    } else {
+      $("#card").append(greeting);
+      $("#greeting, #inputgreeting").hide();
+    }
+  });
+
+  function readURL(input){
+    if(input.files && input.files[0]){
+      var reader = new FileReader();
+
+      reader.onload = function(e){
+        $("#preview").attr("src", e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#inputimage").click(function(){
+    var image = $("#picture");
+    readURL(image);
+  });
+});
