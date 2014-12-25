6344c361751b34f01f2ffe28cc9ef9f4ba0bf291
Stacey Lin <twstacey@gmail.com>
2014-12-25 15:47:50 -0700

    Fix video hiding for Safari

--- public/lightsoff/lightsoff.js
+++ public/lightsoff/lightsoff.js
@@ -36,7 +36,7 @@ function lights(){
 function disappear(){
   document.getElementById('monster').style.visibility = 'hidden';
   document.getElementById('story').innerHTML = "Oops! Looks like you weren't fast enough...<br><h2>Inspired by Lights Out, a short film.";
-  document.getElementById('video').style.visibility = 'visible';
+  document.getElementById('video').style.display = 'block';
   document.getElementById('tip').style.visibility = 'visible';
 }
 
