3873cf49f49501afad3e743d6d12f826f74fdf92
Stacey Lin <twstacey@gmail.com>
2014-12-31 01:44:20 -0700

    Added support for transformations

--- public/wonderlandbunny/scripts.js
+++ public/wonderlandbunny/scripts.js
@@ -6,13 +6,19 @@ $(document).ready(function() {
   $("#drink").click(function() {
     $("#result").html("");
     $("#bunny").css({"-webkit-transform": ""});
+    $("#bunny").css({"-moz-transform": ""});
+    $("#bunny").css({"-o-transform": ""});
+    $("#bunny").css({"transform": ""});
     bunnywidth = parseInt($("#bunny").css("width"), 10);
     var newWidth = bunnywidth + 30;
     if (newWidth >= 300){
       $("#bunny").css("width", 500);
       $("#result").css("font-size", "45px");
       $("#result").html("Biggest bunny possible!");
+      $("#bunny").css({"-moz-transform": "rotateX(-30deg)", "height": 800});
+      $("#bunny").css({"-o-transform": "rotateX(-30deg)", "height": 800});
       $("#bunny").css({"-webkit-transform": "rotateX(-30deg)", "height": 800});
+      $("#bunny").css({"transform": "rotateX(-30deg)", "height": 800});
      leanforward = true;
     } else {
       $("#bunny").css("width", newWidth);
@@ -23,12 +29,18 @@ $(document).ready(function() {
     if (leanforward === true){
       $("#result").html("");
       $("#bunny").css({"-webkit-transform": "rotateX(0deg)", "width": 300, "height" : ""});
+      $("#bunny").css({"-moz-transform": "rotateX(0deg)", "width": 300, "height" : ""});
+      $("#bunny").css({"-o-transform": "rotateX(0deg)", "width": 300, "height" : ""});
+      $("#bunny").css({"transform": "rotateX(0deg)", "width": 300, "height" : ""});
       leanforward = false;
     }
     bunnywidth = parseInt($("#bunny").css("width"), 10);
     newWidth = bunnywidth - 30;
     if (newWidth <= 100){
       $("#bunny").css({"-webkit-transform": "rotateX(20deg)"});
+      $("#bunny").css({"-moz-transform": "rotateX(20deg)"});
+      $("#bunny").css({"-o-transform": "rotateX(20deg)"});
+      $("#bunny").css({"transform": "rotateX(20deg)"});
       $("#result").css("font-size", "10px");
       $("#result").html("Smallest bunny possible!");
     } else {
