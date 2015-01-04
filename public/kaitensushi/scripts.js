cce6df166cdf8d09bfb8d34f8018d85e1e6cdec5
Stacey Lin <twstacey@gmail.com>
2015-01-04 15:32:57 -0800

    Changed colors, added order buttons, moved soy sauce bottle

--- public/kaitensushi/scripts.js
+++ public/kaitensushi/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var sushiQueue = [];
 
   $(".item").click(function(){
-    if (sushiCount <= 5){
+    if (sushiCount < 5){
       var ordered = this.id;
       sushiQueue.push(ordered);
       sushiCount = sushiCount + 1;
@@ -16,6 +16,10 @@ $(document).ready(function() {
 
   function addSushi(item){
     var sushi = $("<img src='" + item + ".png' class='food " + item + "' />");
+    sushi.click(function(){
+      $(this).remove();
+      sushiCount = sushiCount - 1;
+    });
     $(".sushicontainer").append(sushi);
     sushi.animate({"left":"0px"}, 33500, "linear");
   }
