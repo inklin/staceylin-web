c34c196dae0ed55c12752e6bc70248b3eec81b0d
Stacey Lin <twstacey@gmail.com>
2015-01-04 21:02:42 -0800

    Rotated faces of the cube to make a 3D cube

--- public/inspirationalcube/scripts.js
+++ public/inspirationalcube/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
   
+var xAngle = 0;
+var yAngle = 0;
+
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 37:
+      break;
+    case 38:
+      break;
+    case 39:
+      break;
+    case 40:
+      break;
+  }
+};
+
 
 });
