641d0a3cc2621de079df83a3432a108268d52483
Stacey Lin <twstacey@gmail.com>
2015-01-04 23:27:49 -0800

    Added button for mobile, support for different browsers

--- public/inspirationalcube/scripts.js
+++ public/inspirationalcube/scripts.js
@@ -24,7 +24,54 @@ document.onkeydown = function (e) {
   }
 
   $("#cube").css("-webkit-transform", "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
+  $("#cube").css("-moz-transform", "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
+  $("#cube").css("-o-transform", "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
+  $("#cube").css("transform", "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
 };
 
+var clicks = 0;
+
+$(".next").click(function(){
+  var face = clicks % 6;
+  switch (face){
+    case 0:
+      $("#cube").css("-webkit-transform", "rotateY(90deg)");
+      $("#cube").css("-moz-transform", "rotateY(90deg)");
+      $("#cube").css("-o-transform", "rotateY(90deg)");
+      $("#cube").css("transform", "rotateY(90deg)");
+      break;
+    case 1:
+      $("#cube").css("-webkit-transform", "rotateY(180deg)");
+      $("#cube").css("-moz-transform", "rotateY(180deg)");
+      $("#cube").css("-o-transform", "rotateY(180deg)");
+      $("#cube").css("transform", "rotateY(180deg)");
+      break;
+    case 2:
+      $("#cube").css("-webkit-transform", "rotateY(270deg)");
+      $("#cube").css("-moz-transform", "rotateY(270deg)");
+      $("#cube").css("-o-transform", "rotateY(270deg)");
+      $("#cube").css("transform", "rotateY(270deg)");
+      break;
+    case 3:
+      $("#cube").css("-webkit-transform", "rotateX(90deg)");
+      $("#cube").css("-moz-transform", "rotateX(90deg)");
+      $("#cube").css("-o-transform", "rotateX(90deg)");
+      $("#cube").css("transform", "rotateX(90deg)");
+      break;
+    case 4:
+      $("#cube").css("-webkit-transform", "rotateX(-90deg)");
+      $("#cube").css("-moz-transform", "rotateX(-90deg)");
+      $("#cube").css("-o-transform", "rotateX(-90deg)");
+      $("#cube").css("transform", "rotateX(-90deg)");
+      break;
+    case 5:
+      $("#cube").css("-webkit-transform", "rotateX(0deg) rotateY(0deg)");
+      $("#cube").css("-moz-transform", "rotateX(0deg) rotateY(0deg)");
+      $("#cube").css("-o-transform", "rotateX(0deg) rotateY(0deg)");
+      $("#cube").css("transform", "rotateX(0deg) rotateY(0deg)");
+      break;
+  }
+  clicks = clicks + 1;
+});
 
 });
