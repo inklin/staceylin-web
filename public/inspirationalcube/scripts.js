62dbbbeda23bf6cb7f03885765dfa8ae9601ee08
Stacey Lin <twstacey@gmail.com>
2015-01-04 22:20:12 -0800

    Added quotes and colors to sides, fixed back face for quote

--- public/inspirationalcube/scripts.js
+++ public/inspirationalcube/scripts.js
@@ -5,15 +5,25 @@ var yAngle = 0;
 
 document.onkeydown = function (e) {
   switch (e.keyCode) {
+    /* left */
     case 37:
+      yAngle = yAngle - 90;
       break;
+    /* up */
     case 38:
+      xAngle = xAngle + 90;
       break;
+    /* right */
     case 39:
+      yAngle = yAngle + 90;
       break;
+    /* down */
     case 40:
+      xAngle = xAngle - 90;
       break;
   }
+
+  $("#cube").css("-webkit-transform", "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
 };
 
 
