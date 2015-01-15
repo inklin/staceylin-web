59e2dee5ba3d3dfecdb7b4fe17148cd866ee9802
Stacey Lin <twstacey@gmail.com>
2015-01-14 21:08:25 -0800

    Dog woofs in response to ringing phone being clicked on, added Day 26 to Index

--- public/friendly-dog/scripts.js
+++ public/friendly-dog/scripts.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
   
+  setTimeout(startRinging, 5000);
+  var isRinging = false;
+  var phoneInterval;
+
+  function startRinging() {
+    $(".phone-container").addClass("phone-ringing");
+    isRinging = true;
+  }
+
+  $(".phone-container").click(function() {
+    if (isRinging === true){
+      $(".text-bubble").css({"visibility": "visible"});
+      $(".phone-container").removeClass("phone-ringing");
+    }
+    isRinging = false;
+    setTimeout(function(){$(".text-bubble").css({"visibility": "hidden"});}, 2000);
+    setTimeout(startRinging, 7000);
+  });
+
 
 });
