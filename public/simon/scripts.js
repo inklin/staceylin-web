4006db4faea5418822c2f31f9bddbab416103985
Stacey Lin <twstacey@gmail.com>
2015-01-18 00:26:00 -0800

    Added makePattern, showPattern, and lightButton functions

--- public/simon/scripts.js
+++ public/simon/scripts.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
-  
+  var colorChoices = ["red", "green", "blue", "yellow"];
+  var gamePattern = [];
+  var allowUserInput = false;
+
+  function makePattern(){
+    var colorIndex = Math.floor((Math.random() * 4));
+    var color = colorChoices[colorIndex];
+    gamePattern.push(color);
+  }
+
+  function showPattern(){
+    if (gamePattern.length){
+      for (i = 0; i < gamePattern.length; i++){
+        var buttonColor = gamePattern[i];
+        setTimeout(lightButton(buttonColor), i * 1000);
+      }
+
+    setTimeout(function() {
+      allowUserInput = true;
+      }, gamePattern.length * 1000);
+  }
+
+  function lightButton(color) {
+    $('#' + color).addClass(color + "-glow");
+    setTimeout(function() {
+      $('#' + color).removeClass(color + "-glow");
+    }, 200);
+  }
 
 });
