4a4cdba0a5db21f1c9ebdc76ca3ea26d42056b3f
Stacey Lin <twstacey@gmail.com>
2015-01-19 18:03:14 -0800

    added Animate Mole and Pop Mole functions

--- public/whack-a-mole/scripts.js
+++ public/whack-a-mole/scripts.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
-  
+
+  $("#start-button").click(function(){
+    $(".start-game").hide();
+  });
+
+  function animateMole(timeShown, frequency){
+    setInterval(function(){
+      var number = Math.floor(Math.random() * 8 + 1);
+      popMole(number, timeShown);
+    }, frequency);
+  }
+
+  function popMole(number, timeShown){
+    $("#mole-" + number).addClass("up");
+    setTimeout(function(){
+      $("#mole-" + number).removeClass("up");
+    }, timeShown);
+  }
+
+  animateMole(1000, 1500);
 
 });
