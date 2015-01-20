822b370c473bff10a7273a6eb2848176c04dc794
Stacey Lin <twstacey@gmail.com>
2015-01-20 13:41:31 -0800

    Added winner, game over and play again buttons

--- public/whack-a-mole/scripts.js
+++ public/whack-a-mole/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var hits = 0;
   var level = 1;
-  var totalMoles = 1000;
+  var totalMoles = 10;
   var missedMoles = 0;
   var gameOver;
   var frequency = 1000;
@@ -19,8 +19,6 @@ $(document).ready(function() {
       while (number === i){
         number = Math.floor(Math.random() * 9 );
       }
-      console.log("number is" + number);
-      console.log("i is" + i);
       i = number;
       popMole(number);
     }, frequency);
@@ -36,6 +34,9 @@ $(document).ready(function() {
       }
       if (missedMoles === totalMoles){
         gameOver = true;
+        $("#level-count-final").html(level);
+        $(".play-again").show();
+        clearInterval(moleInterval);
       }
     }, 4000);
   }
@@ -53,27 +54,29 @@ $(document).ready(function() {
         missedMoles = 0;
         hits = 0;
         frequency = frequency - 100;
-        animateMole(frequency);
-        $("#level-count").html(level);
-        $(".level").show();
-        setTimeout(function(){
-          $(".level").hide();
-        }, 1000);
+        if (frequency === 900){
+          $(".winner").show();
+        } else {
+          animateMole(frequency);
+          $("#level-count").html(level);
+        }
       }
     }
   });
 
-  $("#play-again").click(function(){
+  $(".play-again-button").click(function(){
+    $(".play-again").hide();
     level = 1;
     $("#level-count").html(level);
-    $(".level").show();
-    setTimeout(function(){
-      $(".level").hide();
-    }, 1000);
     missedMoles = 0;
+    $("#missed-count").html(missedMoles);
     hits = 0;
+    $("#hit-count").html(hits);
     frequency = 1000;
-    animateMole(frequency);
+    gameOver = false;
+    setTimeout(function(){
+      animateMole(frequency);
+    }, 1000);
   });
 
 });
