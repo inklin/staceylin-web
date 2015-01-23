77b9c5c2a6e99e7f41545456ca29172f8da416d3
Stacey Lin <twstacey@gmail.com>
2015-01-23 00:00:21 -0800

    Added 5 min break

--- public/pomodoro/scripts.js
+++ public/pomodoro/scripts.js
@@ -1,24 +1,64 @@
 $(document).ready(function() {
+  var pomoStarted = false;
+  var breakStarted = false;
 
   $("#pomo").click(function(){
-    startPomo();
+    if (!breakStarted) {
+      pomoStarted = true;
+      startTimer(25);
+      breakStarted = false;
+    }
+  });
+
+  $("#break").click(function (){
+    if (!pomoStarted){
+      startTimer(5);
+      breakStarted = true;
+      pomoStarted = false;
+    }
   });
 
   var minutes = 0;
   var seconds = 0;
 
-  function startPomo(){
+  function startTimer(limit){
+    $(".time").show();
+    var displayMinutes;
+    var displaySeconds;
+
     $(".spinner").addClass("spinner-animate");
     $(".filler").addClass("filler-animate");
     $(".mask").addClass("mask-animate");
-    setInterval(function(){
+
+    setTimeout(function(){
+      $(".spinner").removeClass("spinner-animate");
+      $(".filler").removeClass("filler-animate");
+      $(".mask").removeClass("mask-animate");
+    }, limit * 60 * 1000);
+
+    var pomoInterval = setInterval(function(){
       seconds = seconds + 1;
       if (seconds === 60){
         seconds = 0;
         minutes = minutes + 1;
       }
-      $("#seconds").html(seconds);
-      $("#minutes").html(minutes);
+
+      if (seconds < 10){
+        displaySeconds = "0" + seconds;
+      } else {
+        displaySeconds = seconds;
+      }
+
+      if (minutes < 10){
+        displayMinutes = "0" + minutes;
+      } else {
+        displayMinutes = minutes;
+      }
+      $("#seconds").html(displaySeconds);
+      $("#minutes").html(displayMinutes);
+      if (minutes === limit){
+        clearInterval(pomoInterval);
+        }
     }, 1000);
 
   }
