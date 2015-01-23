4d4ad87c31cc216f00de8c17676a05497dcb8daf
Stacey Lin <twstacey@gmail.com>
2015-01-22 23:31:46 -0800

    Added time into javascript and pie chart

--- public/pomodoro/scripts.js
+++ public/pomodoro/scripts.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
-  
+
+  $("#pomo").click(function(){
+    startPomo();
+  });
+
+  var minutes = 0;
+  var seconds = 0;
+
+  function startPomo(){
+    $(".spinner").addClass("spinner-animate");
+    $(".filler").addClass("filler-animate");
+    $(".mask").addClass("mask-animate");
+    setInterval(function(){
+      seconds = seconds + 1;
+      if (seconds === 60){
+        seconds = 0;
+        minutes = minutes + 1;
+      }
+      $("#seconds").html(seconds);
+      $("#minutes").html(minutes);
+    }, 1000);
+
+  }
 
 });
