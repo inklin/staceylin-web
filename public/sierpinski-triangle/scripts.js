4809c419f22e883b9833a65f06babaf9a947da16
Stacey Lin <twstacey@gmail.com>
2015-01-25 23:49:22 -0800

    Added middle white triangle

--- public/sierpinski-triangle/scripts.js
+++ public/sierpinski-triangle/scripts.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   var context = canvas.getContext('2d');
 
   /* An empty array which will contain all triangles */
-  var triangles = [];
+  var trgs = [];
 
   /* The dimension of the main triangle */
   var dim = 500;
 
-  var triangle = {
+  var trg = {
     c: "black",
     /* left vertex of the triangle */
     ly: canvas.height,
@@ -20,20 +20,54 @@ $(document).ready(function() {
     ty: canvas.height - (Math.sqrt(3)* dim)/2,
     tx: dim/2,
     /* dimension of the triangle, the sides */
-    di: dim
+    di: dim,
   };
 
-  triangles.push(triangle);
+  trgs.push(triangle);
 
   function iterate_fractal(){
     /* clear the screen for each iteration */
     /* a rectangle with canvas width and height lengths, with starting point at top left corner */
     context.clearRect(0, 0, canvas.width, canvas.height);
-    /* iterate through all the triangles to find the black ones */
-    for (var i in triangles){
-      /* check the color property of the single triangle */
-      if (triangles[i].c === "black"){
 
+    /* iterate through all the triangles to find the black ones */
+    for (var i in trgs){
+      /* check the color property of the single triangle */
+      if (trgs[i].c === "black"){
+
+        /* create the middle white triangle */
+        var trgWhite = {
+          c: "white",
+          /* calculate the mid points of the sides of the triangle */
+          ly: (trg[i].ly + trg[i].ty) /2,
+          lx: (trg[i].lx + trg[i].tx) /2,
+          ry: (trg[i].ry + trg[i].ty) /2,
+          rx: (trg[i].rx + trg[i].tx) /2,
+          /* the top vertex y coordinate plus the height of the big triangle */
+          ty: trig[i].ty + (Math.sqrt(3) * trig[i].dim)/2,
+          /* the top x vertex is the same as the big triangle */
+          tx: trig[i].tx,
+          /* the dimension is half the size of the triangle it is inside */
+          di: triangles[i].di/2,
+        };
+
+        trgs.push(trgWhite);
+
+        /* create the top black triangle */
+        var trgTop = {
+          c: "black",
+          /* The left vertex is the same as the left vertex of the white triangle */
+          ly: (trg[i].ly + trg[i].ty) /2,
+          lx: (trg[i].lx + trg[i].tx) /2,
+          /* The right vertex is the same as the right vertex of the white triangle */
+          ry: (trg[i].ry + trg[i].ty) /2,
+          rx: (trg[i].rx + trg[i].tx) /2,
+          ty:
+          tx;
+          di: triangles[i].di/2,
+        };
+
+        trgs.push(trgTop);
       }
     }
   }
