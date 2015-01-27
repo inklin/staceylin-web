bab8953acf02cfa48b71c254764d470d7e62a19f
Stacey Lin <twstacey@gmail.com>
2015-01-26 16:23:43 -0800

    Added three smaller black triangles, added function to draw the triangle

--- public/sierpinski-triangle/scripts.js
+++ public/sierpinski-triangle/scripts.js
@@ -39,16 +39,16 @@ $(document).ready(function() {
         var trgWhite = {
           c: "white",
           /* calculate the mid points of the sides of the triangle */
-          ly: (trg[i].ly + trg[i].ty) /2,
-          lx: (trg[i].lx + trg[i].tx) /2,
-          ry: (trg[i].ry + trg[i].ty) /2,
-          rx: (trg[i].rx + trg[i].tx) /2,
+          ly: (trgs[i].ly + trgs[i].ty) /2,
+          lx: (trgs[i].lx + trgs[i].tx) /2,
+          ry: (trg[i].ry + trgs[i].ty) /2,
+          rx: (trg[i].rx + trgs[i].tx) /2,
           /* the top vertex y coordinate plus the height of the big triangle */
-          ty: trig[i].ty + (Math.sqrt(3) * trig[i].dim)/2,
+          ty: trigs[i].ty + (Math.sqrt(3) * trigs[i].dim)/2,
           /* the top x vertex is the same as the big triangle */
-          tx: trig[i].tx,
+          tx: trigs[i].tx,
           /* the dimension is half the size of the triangle it is inside */
-          di: triangles[i].di/2,
+          di: trgs[i].di/2,
         };
 
         trgs.push(trgWhite);
@@ -57,19 +57,72 @@ $(document).ready(function() {
         var trgTop = {
           c: "black",
           /* The left vertex is the same as the left vertex of the white triangle */
-          ly: (trg[i].ly + trg[i].ty) /2,
-          lx: (trg[i].lx + trg[i].tx) /2,
+          ly: (trgs[i].ly + trgs[i].ty) /2,
+          lx: (trgs[i].lx + trg[si].tx) /2,
           /* The right vertex is the same as the right vertex of the white triangle */
-          ry: (trg[i].ry + trg[i].ty) /2,
-          rx: (trg[i].rx + trg[i].tx) /2,
-          ty:
-          tx;
-          di: triangles[i].di/2,
+          ry: (trgs[i].ry + trgs[i].ty) /2,
+          rx: (trgs[i].rx + trgs[i].tx) /2,
+          /* The top vertex is the same as the top vertex of the large triangle */
+          ty: trgs[i].ty,
+          tx: trgs[i].tx,
+          /* the dimension is half the size of the triangle which encloses it */
+          di: trgs[i].di/2,
         };
 
         trgs.push(trgTop);
+
+        /* create the bottom left black triangle */
+
+        var trgLeft = {
+          c: "black",
+          /* The left vertex is the same as the left vertex of the large triangle */
+          ly: trgs[i].ly,
+          lx: trgs[i].lx,
+          /* The right vertex is the same as the "top" vertex of the white triangle */
+          ry: trigs[i].ty + (Math.sqrt(3) * trigs[i].dim)/2,
+          rx: trigs[i].tx,
+          /* The top vertex is the same as the left vertex of the white triangle */
+          ty: (trgs[i].ly + trgs[i].ty) /2,
+          tx: (trg[i].rx + trgs[i].tx) /2,
+          di: trgs[i].di/2,
+        };
+
+        trgs.push(trgLeft);
+
+        /* create the bottom right black triangle */
+        var trgRight = {
+          c: "black",
+          /* The left vertex is the same as the top vertex of the white triangle */
+          ly: trigs[i].ty + (Math.sqrt(3) * trigs[i].dim)/2,
+          lx: trigs[i].tx,
+          /* The right vertex is the same as the right vertex of the large triangle */
+          ry: trgs[i].ry,
+          rx: trgs[i].rx,
+          /* The top vertex is the same as the right vertex of the white triangle */
+          ty: (trgs[i].ry + trgs[i].ty) /2,
+          tx: (trgs[i].rx + trgs[i].tx) /2,
+          di: trgs[i].di/2,
+        };
+
+        trgs.push(trgRight);
       }
+
+    drawTrg(trgs[i]);
     }
   }
 
+  function drawTrg(t){
+    ctx.beginPath();
+    ctx.moveTo(t.lx, t.ly);
+    ctx.lineTo(t.rx, t.ry);
+    ctx.lineTo(t.tx, t.ty);
+    ctx.lineTo(t.lx, t.ly);
+
+    ctx.fillStyle = t.c;
+    ctx.fill();
+    ctx.closePath();
+
+  }
+
+iterate_fractal();
 });
