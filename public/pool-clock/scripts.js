be5334353236c799f85598bd86b981eeddd21a2d
Stacey Lin <twstacey@gmail.com>
2015-01-28 21:50:15 -0800

    Renamed clock, added canvas element

---
+++ public/pool-clock/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+  var canvas = document.getElementById('canvas'),
+      ctx = canvas.getContext('2d');
+
+  /* Function to get minutes and seconds */
+  function displayTime(){
+    var now = new Date();
+    var m = now.getMinutes();
+    var s = now.getSeconds();
+  }
+
+
+
+});
