a71537793272b4edc98cff247eab84161e52b1de
Stacey Lin <twstacey@gmail.com>
2015-02-02 23:06:13 -0800

    Set up loop for divs to follow the cursor at different speeds, fast to slow in the back

--- public/follower/scripts.js
+++ public/follower/scripts.js
@@ -5,28 +5,43 @@ $(document).ready(function() {
 
   /* Get coordinates of the mouse when it moves */
   $(document).on('mousemove', function(e){
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+    mouseX = e.pageX - width;
+    mouseY = e.pageY - height;
   });
 
   /* Get coordinates for mobile users */
   document.addEventListener('touchmove', function(e){
     e.preventDefault();
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+    mouseX = e.pageX - width;
+    mouseY = e.pageY - height;
   }, false);
 
-  var follower = $(".follower");
-  moveDiv(follower, 10);
+  var pieces = ["face3", "face2", "face1"];
+  var speed = 10;
+  var width;
+  var height;
+  var name;
 
-  function moveDiv(element, speed){
+  for (var i = 0; i < pieces.length; i++){
+    /* move the div to the cursor */
+    name = pieces[i];
+    width = $("." + name).width() / 2  - 25;
+    height = $("." + name).height() /2 - 25;
+    moveDiv(name, speed, width, height);
+    /* make the one in front of it a little faster */
+    speed = speed - 1;
+  }
+
+  /* Create a function to move the element to the mouse cursor */
+  function moveDiv(element, speed, offsetX, offsetY){
     var xPosition = 0;
     var yPosition = 0;
-    var loop = setInterval(function(){
-      xPosition += (mouseX - xPosition) / speed;
-      yPosition += (mouseY - yPosition) / speed;
-    $(element).css({left: xPosition, top: yPosition});
-  }, 30);
+    var moveLoop = setInterval(function(){
+      console.log("moving " + element + "at speed of " + speed);
+      xPosition += (mouseX - offsetX - xPosition) / speed;
+      yPosition += (mouseY - offsetY- yPosition) / speed;
+      $("." + element).css({left: xPosition, top: yPosition});
+    }, 30);
   }
 
 });
