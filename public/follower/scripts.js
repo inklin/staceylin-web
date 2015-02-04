9536d8a7e30e4a22beb9625acfc02a82a3275c7e
Stacey Lin <twstacey@gmail.com>
2015-02-03 17:32:56 -0800

    Added styling for Follower

--- public/follower/scripts.js
+++ public/follower/scripts.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
     mouseY = e.pageY - height;
   }, false);
 
-  var pieces = ["face3", "face2", "face1"];
-  var speed = 10;
+  var pieces = ["five", "four", "three", "two", "one"];
+  var speed = 20;
   var width;
   var height;
   var name;
@@ -26,10 +26,11 @@ $(document).ready(function() {
     /* move the div to the cursor */
     name = pieces[i];
     width = $("." + name).width() / 2  - 25;
+    console.log("width is " + width + " for " + name);
     height = $("." + name).height() /2 - 25;
     moveDiv(name, speed, width, height);
     /* make the one in front of it a little faster */
-    speed = speed - 1;
+    speed = speed - 3;
   }
 
   /* Create a function to move the element to the mouse cursor */
@@ -37,7 +38,6 @@ $(document).ready(function() {
     var xPosition = 0;
     var yPosition = 0;
     var moveLoop = setInterval(function(){
-      console.log("moving " + element + "at speed of " + speed);
       xPosition += (mouseX - offsetX - xPosition) / speed;
       yPosition += (mouseY - offsetY- yPosition) / speed;
       $("." + element).css({left: xPosition, top: yPosition});
