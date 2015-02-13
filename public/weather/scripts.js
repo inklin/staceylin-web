02b2b92d76de8e5348d46443c6ba0ff3c099d1c6
Stacey Lin <twstacey@gmail.com>
2015-02-12 22:49:11 -0800

    Added icon files, included icons in script, added styling for display

--- public/weather/scripts.js
+++ public/weather/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   var lat;
   var lon;
+  var weather;
+  var temp;
+  var icon;
 
   // send Ajax post request to get longitude and latitude
   $.ajax({
@@ -10,12 +13,10 @@ $(document).ready(function() {
     success: function(location) {
       lon = location.longitude;
       lat = location.latitude;
-      console.log('longitude is ' + lon);
-      console.log('latitude is ' + lat);
     }
   });
 
-  // use longitude and latitude with a center radius to find the weather 
+  // use longitude and latitude to find the weather description and temperature
   function getWeather(callback){
     var weather = '//api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon ;
     $.ajax({
@@ -27,7 +28,28 @@ $(document).ready(function() {
 
   getWeather(function(data){
     console.log('weather data received');
-    console.log(data.weather[0].description);
+    weather = data.weather[0].main;
+    temp = kelvinToCelsius(data.main.temp);
+    icon = data.weather[0].icon;
+    console.log(icon);
+    // if icon is for clouds, point to 03 svg 
+    if (icon === '04n' || icon === '04d' || icon === '03n'){
+      icon = '03';
+    }
+    showWeather();
   });
+
+  // Show weather on the page
+  function showWeather(){
+    $('.display').append('<img src="images/' + icon + '.svg" class="icon" />');
+    $('.description').html(weather);
+    $('.temp').html(temp + ' &#x2103;');
+  }
+
+  // convert kelvin to celsius
+  function kelvinToCelsius(k){
+    var c = Math.floor(k - 273);
+    return c;
+  }
 
 });
