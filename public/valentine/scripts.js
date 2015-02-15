fdeeb25e8231f2025f7d35d9a04f7d5baf5b6411
Stacey Lin <twstacey@gmail.com>
2015-02-14 23:41:00 -0800

    Changed hover to jQuery mouseenter and mouseout

--- public/valentine/scripts.js
+++ public/valentine/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  $('.envelope').on('mouseover', function(){
+    $('.top, .left, .right, .bottom, .card').addClass('hover');
+  });
 
+  $('.envelope').on('mouseout', function(){
+   $('.top, .left, .right, .bottom, .card').removeClass('hover');
+  });
 });
