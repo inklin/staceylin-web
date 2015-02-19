967d2a14f2f825514a2c6c6ec8c12dc087746361
Stacey Lin <twstacey@gmail.com>
2015-02-18 23:12:07 -0800

    Added function to show slider value

--- public/writeordie/scripts.js
+++ public/writeordie/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
   
+  var value;
+
 // Create slider for grace period
   $(".slider").slider({
-    min: 10,
-    step: 10,
+    min: 5,
+    step: 5,
     max: 30,
     value: 10,
+    stop: showValue,
+    
   });
+
+  function showValue(){
+    value = $(".slider").slider("value");
+    $(".grace").html(value + " seconds");
+  }
+
+  function checkTyping(){
+    
+  }
 
 });
