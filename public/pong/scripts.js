7b7fc7423b4f01f1721bbc312cb1d8c3285b086e
Stacey Lin <twstacey@gmail.com>
2015-02-23 00:21:40 -0800

    Added move player paddle by mouse or up and down arrow keys

--- public/pong/scripts.js
+++ public/pong/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   var h = 500;
   var canvas, ctx, keystate;
   var player, ai, ball;
+  var UpKey = 38, DownKey= 40;
 
   player = {
     x: null,
@@ -12,7 +13,12 @@ $(document).ready(function() {
     height: 100,
 
     update: function(){
-
+      if ( keystate[UpKey] ) {
+        this.y -= 5;
+      }
+      if ( keystate[DownKey] ){
+        this.y += 5;
+      }
     },
 
     draw: function(){
@@ -54,6 +60,19 @@ $(document).ready(function() {
     canvas.height = h;
     ctx = canvas.getContext('2d');
 
+    keystate = [];
+    $(document).keydown(function(e){
+      keystate[e.keyCode] = true;
+    });
+
+    $(document).keyup(function(e){
+      delete keystate[e.keyCode];
+    });
+
+    $('canvas').on('mousemove', function(e){
+      player.y = e.pageY - 50;
+    });
+
     init();
 
     var loop = function (){
@@ -83,9 +102,25 @@ $(document).ready(function() {
   }
 
   function draw(){
+    ctx.fillRect(0, 0, w, h);
+    ctx.save();
+
+    ctx.fillStyle = '#FFFFFF';
     player.draw();
     ai.draw();
     ball.draw();
+
+    // Draw the dotted middle line
+    var wi = 4;
+    var x = (w - wi)*0.5;
+    var y = 0;
+    var step = h/15;
+    while (y < h){
+      ctx.fillRect(x, y + step * 0.25, wi, step * 0.5);
+      y = y + step;
+    }
+
+    ctx.restore();
 
   }
 
