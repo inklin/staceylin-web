77bfafd6fff4500cd917e88a7ba5d75b94eb6219
Stacey Lin <twstacey@gmail.com>
2015-02-23 22:50:08 -0800

    Centered game canvas, added function to update y, added function to update x movement, added function to check collision

--- public/pong/scripts.js
+++ public/pong/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   var canvas, ctx, keystate;
   var player, ai, ball;
   var UpKey = 38, DownKey= 40;
+  var pi = Math.PI;
 
   player = {
     x: null,
@@ -50,6 +51,34 @@ $(document).ready(function() {
     update: function(){
       this.x = this.x + ball.velocity.x;
       this.y = this.y + ball.velocity.y;
+
+      // when the y coordinates go too low or too high, change direction
+      if (this.y < 0 || this.y + this.length > h){
+        // if the ball is going upwards add offset so the ball goes to the bottom of the game container
+        var offset = this.velocity.y < 0 ? 0 - this.y : h - (this.y + this.length);
+        this.y += 2 * offset;
+        this.velocity.y *= -1;
+      }
+
+      // return true if the boxes are intersecting
+      var AABBIntersect = function (ax, ay, aw, ah, bx, by, bw, bh){
+        return ax < bx + bw && ay < by + bh && bx < ax + aw && by < ay + ah;
+      };
+
+      // if the ball is going to the left, check against player paddle
+      // else check against the ai paddle (ball going to the right)
+      var paddle = this.velocity.x < 0 ? player : ai;
+      if (AABBIntersect(paddle.x, paddle.y, paddle.width, paddle.height, this.x, this.y, this.length, this.length)){
+        // calculate where the ball hits the paddle compared to the length of the paddle
+        var n = (this.y + this.length - paddle.y)/(paddle.height + this.length);
+        var phi = 0.25 * pi * (2 * n - 1); // Pi / 4 = 45 degrees
+
+        // check if the paddle is the player's or ai
+        // if the ball hits the player paddle, velocity is positive and ball goes to the right
+        // if the ball hits the ai paddle, velocity is negative and the ball goes to the left
+        this.velocity.x =  (paddle === player ? 1 : -1) * this.speed * Math.cos(phi);
+        this.velocity.y = this.speed * Math.sin(phi);
+      }
     },
     draw: function(){
       ctx.fillRect(this.x, this.y, this.length, this.length);
@@ -97,8 +126,8 @@ $(document).ready(function() {
     ball.y = (h - ball.length)/2;
 
     ball.velocity = {
-      x: 0,
-      y: ball.speed,
+      x: ball.speed,
+      y: 0,
     };
   }
 
@@ -110,6 +139,7 @@ $(document).ready(function() {
   }
 
   function draw(){
+    // Draw the black background and save
     ctx.fillRect(0, 0, w, h);
     ctx.save();
 
