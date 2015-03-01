69f1313010ccbc484f99c7444baf8a705ae9a1ac
Stacey Lin <twstacey@gmail.com>
2015-03-01 00:22:40 -0800

    Put image in the center, added color styles to page

--- public/origami/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  
-  var canvas = document.getElementById('canvas'), 
-      ctx = canvas.getContext('2d');
-
-      canvas.width = 800;
-      canvas.height = 600;
-});
