aed7ee3d41fd718c07fa9ff6a009b87b627bfd39
Stacey Lin <twstacey@gmail.com>
2015-02-28 23:16:36 -0800

    Added shapes for top

---
+++ public/origami/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  
+  var canvas = document.getElementById('canvas'), 
+      ctx = canvas.getContext('2d');
+
+      canvas.width = 800;
+      canvas.height = 600;
+});
