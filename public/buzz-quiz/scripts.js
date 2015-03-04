2dfdf81ce73cc4d11b5012ed6ca61f1787ecbe84
Stacey Lin <twstacey@gmail.com>
2015-03-03 23:41:20 -0800

    Added styles and gifs for results, added styles for navigation

--- public/buzz-quiz/scripts.js
+++ public/buzz-quiz/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   
   var description = [];
-  description.push('You come from humble beginnings but dream of finding your true self. You may fall in with the wrong crowd but good friends and support from will family keep you grounded. You are an honest and brave individual.');
-  description.push('You are a romantic at heart. Family is very important to you and you are fiercely protective of those you care about. You have empathy for others and do not let society dictate who you love.');
-  description.push('You have a gentle soul and believe that cleanliness is close to godliness. Some may be jealous of the attention you get, but you are a popular person who has many guy friends.');
+  description.push('You come from humble beginnings but dream of finding your true self. You may fall in with the wrong crowd but good friends and love from family will keep you grounded. You are an honest and brave individual.');
+  description.push('You are a romantic at heart. Family is very important to you and you are fiercely protective of those you care about. You have empathy for others and do not let society dictate who you associate with.');
+  description.push('You have a gentle soul and believe that cleanliness is close to godliness. Some may be jealous of the attention you get, but you are a popular person who has many guy friends. Keep on being happy-go-lucky!');
   description.push('You were born for greatness. Although you carry big expectations on your shoulders, you know how to let loose and relax every once in a while. You have a small group of friends who are always up for a little singing and dancing. Hakuna matata!');
 
   var movies = ['Pinocchio', 'Lady & the Tramp', 'Snow White & the Seven Dwarves', 'The Lion King'];
@@ -56,7 +56,8 @@ $(document).ready(function() {
         title = movies[3];
         break;
     }
-      $('.result').html('<p class="title">You chose <span class="chosen-title">' + title + '</span> as your favourite.</p><img src="imgs/' + movie + '.gif" class="gif" /><p class="description">'+ result + '</p>');
+      $('.result').show();
+      $('.result').html('<h2>You chose <span class="chosen-title">' + title + '</span></h2><img src="imgs/' + movie + '.gif" class="gif" /><p class="description">'+ result + '</p>');
 
   }
 
