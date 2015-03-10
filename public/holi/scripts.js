669805f757d8d693c0ffc821d0f8f7dd0dc134bb
Stacey Lin <twstacey@gmail.com>
2015-03-09 23:59:32 -0700

    Added requestAnimFrame callback, draw function, animLoop for splatter effect

--- public/holi/scripts.js
+++ public/holi/scripts.js
@@ -5,22 +5,29 @@ $(document).ready(function() {
   var particles = [];
   var w = $(window).width();
   var h = $(window).height();
+  var colors = ['#FF00FF', '#FFFF00', '#00CCFF', '#00CC00', '#6600CC', '#FF6600', '#FF9933'];
+  var colorIndex = 0;
+  color = colors[colorIndex];
 
   canvas.width = w;
   canvas.height = h;
 
   canvas.onmousedown = function(e){
 
-    for (var i = 0; i < 36 * 2; i ++){
+    color = colors[colorIndex];
+
+    console.log('mouse pressed');
+    for (var i = 0; i < 70; i ++){
       particles.push({
         x: e.pageX,
         y: e.pageY,
         angle: i * 5,
-        size: 5 + Math.random() * 3,
-        life: 200 + Math.random() * 50,
+        size: 5 + Math.random() * 2,
+        life: 150 + Math.random() * 100,
       });
     }
   };
+
 
   var delta = 0;
   var last = Date.now();
@@ -40,11 +47,41 @@ $(document).ready(function() {
         p.life = 0;
       }
 
-      if (p.life <=0){
+      if (p.life <= 0){
         particles.splice(i--, 1);
         continue;
       }
     }
   }
+
+  function draw(){
+    ctx.fillStyle = color;
+    for (var i = 0; i < particles.length; i++){
+      if (Math.random() < 0.1){
+        continue;
+      }
+
+      var p = particles[i];
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2, false);
+      ctx.fill();
+    }
+  }
+
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+(function animloop(){
+    requestAnimFrame(animloop);
+    animate();
+    draw();
+})();
+
 
 });
