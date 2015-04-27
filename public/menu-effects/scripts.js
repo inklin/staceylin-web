b3ebab570ba79201e756295f5da5678af64a0e4e
Stacey Lin <twstacey@gmail.com>
2015-04-27 13:24:59 -0700

    Added drop down and scale up and down effects

--- public/menu-effects/scripts.js
+++ public/menu-effects/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var effect;
 
   function init(){
-    for (var i = 1; i < 8; i++){
+    for (var i = 1; i < 10; i++){
       var menu = "<div class='menu-wrapper effect-" + i + "' id='menu-effect" + i + "'><h2>Menu</h2><ul><li><a href='#'>About</a></li><li><a href='#'>Blog</a></li><li><a href='#'>FAQ</a></li><li><a href='#'>Contact</a></li></ul></div>";
       $('body').append(menu);
     }
