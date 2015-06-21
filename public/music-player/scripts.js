2b9499917b6811c21f591eb2a7e426eb81a63562
Stacey Lin <twstacey@gmail.com>
2015-06-20 18:23:14 -0700

    Auto load first song in audio player

--- public/music-player/scripts.js
+++ public/music-player/scripts.js
@@ -11,11 +11,13 @@ $(document).ready(function() {
   if (isSupported){
     var index = 0;
     var playing = false;
+    var mediaPath = 'song-files/';
     var tracks = [
       {'number': 1, 'title': 'Song Number One', 'file': 'Song_1'},
       {'number': 2, 'title': 'Song Number Two', 'file': 'Song_2'},
       {'number': 3, 'title': 'Song Number Three', 'file': 'Song_3'}
     ];
+    var extension = '.mp3';
 
     var trackCount = tracks.length;
 
@@ -28,9 +30,29 @@ $(document).ready(function() {
     }).bind('ended', function(){
       // event listener for media event ended
       // check to see if the audio is the last song
+      if ((index + 1) < trackCount){
+        index ++;
+        loadSong(index);
+        audio.play();
+      } else {
+        audio.pause();
+        index = 0;
+        loadSong(index);
+      }
+    }).get(0);
 
+    var loadSong = function(id){
+      audio.src = mediaPath + tracks[id].file + extension;
+      audio.load();
+    };
 
-    });
+    var playSong = function(id){
+      loadSong(id);
+      audio.play();
+    };
+
+    // Load the first song to start
+    loadSong(index);
   }
 
 });
