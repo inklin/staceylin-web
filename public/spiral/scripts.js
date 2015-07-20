50ccde3dac6737de54101f8c5d1a703d6eff345f
Stacey Lin <twstacey@gmail.com>
2015-07-19 21:13:07 -0700

    Style and spiral shape changes

--- public/spiral/scripts.js
+++ public/spiral/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   
-  var shapeArray = [1, 5, 8, 10, 12, 15, 20, 24, 30, 40, 48];
+  var shapeArray = [1, 6, 12, 11, 15, 18, 20, 24, 30, 40, 48, 60];
   var shapeIndex = 0;
   var shapeMaxIndex = shapeArray.length;
   var incrementSize = shapeArray[shapeIndex];
+  var modalHidden = false;
+
+  // Draw the spiral
 
   function drawSpiral(){
     var canvas = document.getElementById('canvas');
@@ -20,14 +23,13 @@ $(document).ready(function() {
     var y = canvas.height/2;
 
     var stepsPerRotation = 60;
-    
-    ctx.beginPath();
-    ctx.moveTo(x, y);
     var increment =  incrementSize * Math.PI / stepsPerRotation;
     var angle = increment;
-
-    var totalRotations = 20;
+    var totalRotations = 15;
     var totalRotationAngle = totalRotations * 2 * Math.PI;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
 
     while (angle < totalRotationAngle){
       x = x + angle * Math.cos(angle + counter);
@@ -39,9 +41,9 @@ $(document).ready(function() {
     ctx.stroke();
   }
 
-  var counter = 0;
-  var modalHidden = false;
 
+  // Animate the spiral
+  var counter = 0;
   function animateSpiral(){
     drawSpiral(counter);
     counter += 0.02;
@@ -50,6 +52,7 @@ $(document).ready(function() {
 
   window.requestAnimationFrame(animateSpiral);
 
+  // Change shape on click on space bar press
   $(document).click(function(){
     changeShape();
   });
@@ -61,6 +64,7 @@ $(document).ready(function() {
     }
   });
 
+  // Change the increment size for a different spiral shape
   function changeShape(){
     if (!modalHidden) {
       $('.modal').fadeOut();
@@ -68,6 +72,7 @@ $(document).ready(function() {
     } else {
       shapeIndex = ( shapeIndex + 1 ) % shapeMaxIndex;
       incrementSize = shapeArray[shapeIndex];
+      console.log(incrementSize);
     }
   }
 
