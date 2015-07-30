41fb4d20e8bc3bfd871c66564456a0d0ba1131fe
Stacey Lin <twstacey@gmail.com>
2015-07-30 12:09:53 -0700

    Changed directoriy name

---
+++ public/lightning/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  // Initiate Canvas
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var width = 800;
+  var height = 500;
+  canvas.width = width;
+  canvas.height = height;
+
+
+});
